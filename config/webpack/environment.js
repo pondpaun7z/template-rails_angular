cbe6ab66ec26253e79e8e0388fe644ef5da2eafc
Pondpaun7z <pppangpondpp7@gmail.com>
2018-08-12 18:05:52 +0700

    Added load html template

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,8 @@
 const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
+const html =  require('./loaders/html')
 
 environment.loaders.append('typescript', typescript)
+environment.loaders.insert('html', html)
+
 module.exports = environment
